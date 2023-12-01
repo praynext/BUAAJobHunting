e10c5ae7ec61eeba69c11a138a32ef73143d9d0e
praynext <ojyourfather@163.com>
2023-12-01 09:02:04 +0800

    fix时间问题

--- main.go
+++ main.go
@@ -22,6 +22,7 @@ import (
 	"net/smtp"
 	"os"
 	"path"
+	"time"
 )
 
 func InitSql(Host string, Port int, User string, Password string, Database string) {
@@ -110,6 +111,8 @@ func LoadConfig() {
 	InitUpGrader()
 	InitCron()
 	docs.SwaggerInfo.BasePath = viper.GetString("DocsPath")
+	timeLocal, _ := time.LoadLocation("Asia/Beijing")
+	time.Local = timeLocal
 }
 
 // @title BUAAJobHunting Backend API
